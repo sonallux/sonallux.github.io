1e9097b6addc050e88e11da8f1bd6873494fe57a
sonallux <13821543+sonallux@users.noreply.github.com>
2023-03-15 21:23:55 +0100

    Remove clsx dependency

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import clsx from 'clsx';
 import styles from './styles.module.css';
 
 const FeatureList = [
@@ -37,7 +36,7 @@ const FeatureList = [
 
 function Feature({Svg, title, description}) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className="col col--4">
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>

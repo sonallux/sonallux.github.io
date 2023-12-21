79f7e753db03d313494359dd4d8db29f4b378f9b
sonallux <sonallux@gmx.de>
2023-12-21 14:24:46 +0100

    Remove unused docusaurus images

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
 
@@ -17,7 +16,7 @@ function HomepageHeader() {
           Welcome to my personal website! My name is Jonas, and I am a full-stack software engineer. With years of experience in both front-end and back-end development, I am capable of delivering high-quality, scalable software solutions. I'm also passionate about open source software and have contributed to several projects.
         </p>
         <p>
-          Please feel free to explore and don't hesitate to get in touch if you have any questions or inquiries. Thank you for visiting!
+          Please feel free to explore and don't hesitate to get in touch if you have any questions. Thank you for visiting!
         </p>
         <div className={styles.buttons}>
           <Link
@@ -43,11 +42,8 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+    <Layout>
       <HomepageHeader />
       {/*<main>
         <HomepageFeatures />

87a0b252acb495dd6621f2a408dc5138ff5efc74
sonallux <sonallux@gmx.de>
2025-04-10 21:19:50 +0200

    fix: adjust YouTubeVideo component

--- src/components/YouTubeVideo.js
+++ src/components/YouTubeVideo.js
@@ -3,7 +3,8 @@ import React from "react";
 export default function YouTubeVideo({videoId, title}) {
   const fullUrl = `https://www.youtube.com/embed/${videoId}`
   return (
-    <iframe width="448" height="252" src={fullUrl} title={title}
+    <iframe width="384" height="216" src={fullUrl} title={title}
+            style={{backgroundColor: "black"}}
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
   );
